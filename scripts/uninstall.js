e27ced9a3fd7f4391226d0844089cc33201cf76e
Juan Manuel Scilingo <juanscilingo@gmail.com>
2019-07-24 15:35:58 -0300

    Adds log to uninstall script

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -9,8 +9,9 @@ var svc = new Service({
 // Listen for the "uninstall" event so we know when it's done.
 svc.on('uninstall',function(){
   console.log('Uninstall complete.');
-  console.log('The service exists: ',svc.exists);
+  console.log('The service exists: ', svc.exists);
 });
 
 // Uninstall the service.
+console.log('Uninstalling service...');
 svc.uninstall();
