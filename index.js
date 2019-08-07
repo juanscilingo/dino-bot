873aac750edc1cf38669bfef80d55db9bd086893
Martin Galindez <galindez.martin@hotmail.com>
2019-08-06 22:07:47 -0300

    filters demolished lines and makes the other ones look prettier

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const tail = new Tail(path);
 tail.on("line", async line => {
   console.log(line)
   try {
-    await axios.post(process.env.DISCORD_WEBHOOK_URL, {content: `\`\`\`${line}\`\`\``})
+    var prettierLine = line.match(/(\d{2}:\d{2}:\d{2}).\d{4} Tribe xulapa tribe, ID \d*: (Day \d+, .+: )(?:<RichColor.*>(.*?)<\/>|(.*))/);
+    if (prettierLine && prettierLine.length > 4) line = prettierLine[1] + ' - ' + prettierLine[2] + (prettierLine[3] ? prettierLine[3] : prettierLine[4]);
+
+    if (!line.includes("demolished"))
+      await axios.post(process.env.DISCORD_WEBHOOK_URL, {content: `\`\`\`${line}\`\`\``})
   } catch (err) {
     console.log(err)
   }
